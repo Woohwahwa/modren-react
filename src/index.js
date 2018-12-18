09f101f0390f247f887e865cced543da9317eae5
Cindy <1233wsh@gmail.com>
2018-12-19 01:52:15 +0900

    ~Ch02. 17

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import ReactDOM from 'react-dom';
 
 // Create a react component
 const App = () => {
-    return (
-        <div>
-            <label class="label" for="name">
-                Enter name:
-            </label>
-            <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>
-                Submit
-            </button>
-        </div>
-    );
+  return (
+    <div>
+      <label className="label" form="name">
+        Enter name:
+      </label>
+      <input id="name" type="text"/>
+      <button style={{ backgroundColor: 'blue', color: 'white' }}>
+        Submit
+      </button>
+    </div>
+  );
 };
 
 // Take the react component and show it on the screen
 ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
+  <App />,
+  document.querySelector('#root')
 );
